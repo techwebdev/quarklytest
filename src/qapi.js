256811f273feb805e70b44d20969243155706607
techwebdev <63657518+techwebdev@users.noreply.github.com>
2020-11-08 01:10:01 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa6f76d79530b001fd1f629",
+				"5fa6f76d79530b001fd1f62b"
+			]
+		},
+		"5fa6f76d79530b001fd1f629": {
+			"id": "5fa6f76d79530b001fd1f629",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa6f76d79530b001fd1f62b": {
+			"id": "5fa6f76d79530b001fd1f62b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
